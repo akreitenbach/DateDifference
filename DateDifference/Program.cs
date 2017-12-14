4fea228ff84b15d317437f6efe09569ea944bd93
Angie Reitenbach <akreitenbach@gmail.com>
2017-12-13 19:12:02 -0600

    Uploading DateDifference

---
+++ DateDifference/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace DateDifference
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the first day of school:");
+            var date1 = Console.ReadLine();
+            //convert to Date
+            var schoolStartDate = DateTime.Parse(date1);
+            //Console.WriteLine(schoolStartDate);
+
+            Console.Write("Enter today's date:");
+            var date2 = Console.ReadLine();
+            //convert to Date
+            var today = DateTime.Parse(date2);
+            //Console.WriteLine(today);
+
+            //Calculate remaining days of summer vacation
+            var daysSummerVacation = (schoolStartDate - today).TotalDays;
+            Console.WriteLine($"There are {daysSummerVacation} days remaing of summer vacation.  Enjoy!");
+            
+            Console.ReadLine();
+        }
+    }
+}
